29a9dc5d28864239981d9124bc3f5e1c3337108b
Tashila <tashila.dev@gmail.com>
2022-10-02 19:24:06 +0400

    Initiated custom map object

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap'
 
 const user = new User();
 const company = new Company();
+const customMap = new CustomMap();
